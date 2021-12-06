7a2815bb352f5354d40699cbabe6ff15b50b0ca8
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2021-12-07 00:12:58 +0530

    Create comment.js

---
+++ comment.js
@@ -0,0 +1,9 @@
+//This is first comment
+console.log('My mother drove me to the airport with the windows rolled down.');
+
+/* This is a multi-line comment.
+console.log('It was seventy-five degrees in Phoenix, the sky a perfect, cloudless blue.');
+console.log('I was wearing my favorite shirt — sleeveless, white eyelet lace');
+console.log('I was wearing it as a farewell gesture.');
+console.log('My carry-on item was a parka.');
+*/
