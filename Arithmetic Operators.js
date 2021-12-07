0672fd773e4e01dad65b4f4d050d5611065cb999
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2021-12-07 21:19:39 +0530

    Create Arithmetic Operators.js

---
+++ Arithmetic Operators.js
@@ -0,0 +1,4 @@
+console.log(31+3.5); //This is the age i’ll be when we start sending people to live on Mars.
+console.log(2021-1969); //Current year subtract 1969 is how many years it’s been since the 1969 moon landing.
+console.log(65/240);
+console.log(0.2708*100); //That’s the percent of the sun that is made up of helium. Assuming we could stand on the sun, we’d all sound like chipmunks!
