f7df7739afa989ccae3a72fd4a1381602cfdc319
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2022-01-23 22:14:04 +0530

    Create Methods.js

---
+++ Methods.js
@@ -0,0 +1,6 @@
+console.log('Now this looks like a job for me,so everybody just follow me'.toUpperCase());
+console.log(' cause we need a little conroversy.But it feels so empty without me '.trim());  //I'm using different methods to call and logging them to console object 
+console.log('We are like butterflies who flutter for a day and think it is forever'.startsWith()); //I'mtaking all the methods from JavaScript String Documentation from MDN
+console.log('We are like butterflies who flutter for a day and think it is forever'.startsWith('G'));
+console.log('We are like butterflies who flutter for a day and think it is forever'.startsWith('W'));
+console.log('We are like butterflies who flutter for a day and think it is forever'.startsWith('w'));
