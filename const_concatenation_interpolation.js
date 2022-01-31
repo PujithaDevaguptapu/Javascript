1aff373aebf19152abcc4f3b0cc6be4168af2108
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2022-01-31 19:56:37 +0530

    Create const_concatenation_interpolation.js

---
+++ const_concatenation_interpolation.js
@@ -0,0 +1,11 @@
+const myField = 'Cybersecurity'; //This is constant variable
+console.log(myField);
+myField = 'Research'; //Shows Type Error
+const yourField;  //Shows syntax Error
+console.log(yourField);
+//String Concatenation
+let foodToEat = 'pizza';
+console.log('Food i want to eat ' + foodToEat + '.');
+//String Interpolation
+let switchOn = true;
+console.log(`The light bulb lights up the room ${switchOn}. But it does not light up my heart`);  
