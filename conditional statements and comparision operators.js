192bc7429d4455f9b28e6be376123516084e21d6
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2022-02-11 00:19:11 +0530

    Create conditional statements and comparision operators.js

---
+++ conditional statements and comparision operators.js
@@ -0,0 +1,21 @@
+let dryFruit = false;
+dryFruit = true;
+if(dryFruit){
+  console.log('Are dryfruits available today?'); //Trying out conditional statements - If statement 
+}
+
+let milk = false;
+if(milk) {
+  console.log(`Are you trying to eat a sweet:${milk}`);
+}
+else{
+  console.log('I like to eat as much as i can'); //Example of an conditional statements - If..Else statements
+}
+
+let hungerLevel = 7;
+if (hungerLevel>7){
+  console.log('Time to eat!');
+}
+else{
+  console.log('We can eat later!'); //Example of a comparision operator
+}
